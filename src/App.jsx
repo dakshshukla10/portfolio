f0f55b7fee2018bfc426e1e9301b27e692d426a7
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-07-06 20:10:39 +0530

    feat:Added Logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'; {/*This is like getting your roads for your Lego city. It's going to allow us to move between different parts of your website.*/}
 
 import {About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas} from './components';
 
-const App = () => {
+const App = () => { {/*This is where you start building your Lego city. 'App' is your whole city*/}
   return (
-    <BrowserRouter>
+    <BrowserRouter> {/*This is like getting your roads for your Lego city. It's going to allow us to move between different parts of your website.*/}
       <div className="relative z-0 bg-primary">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
           <Navbar />
@@ -24,4 +24,4 @@ const App = () => {
   )
 }
 
-export default App
+export default App; {/* Finally, when your city is built, you open it up for people to visit. This line is saying, "My city is ready, and anyone can come to see it!" */}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'; {/*This is like getting your roads for your Lego city. It's going to allow us to move between different parts of your website.*/}
+import {Link} from 'react-router-dom';
+
+import{styles} from '../styles';
+import{navLinks} from '../constants';
+import{logo,menu,close} from '../assets';
 
 const Navbar = () => {
+  const [active,setActive] = useState('');
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/" className="flex items-center gap-2" onClick={()=> {
+          setActive("");
+          window.scrollTo(0,0);
+        }}>
+          <img src={logo} alt="lo go" className="w-9 h-9 object-contain"/>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
