b756de07ce1da55d7c8e0f51f714781fb9b7fabd
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-08-16 23:34:10 +0530

    Fix Navbar , Add click and highlight feature to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
           {navLinks.map((link) => (
-            <li key={link.id}>
+            <li key={link.id} className={`${active===link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
