cf8b1b20e81b7b7324dcb6c9a260392bf9edb893
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-08-17 14:47:58 +0530

    Feat:Navbar completed and functional

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           }}
             >
               <img src={logo} alt="logo" className="w-9 h=9 object-contain" />
-              <p className="text-white text-[18px] font-bold cursor-pointer">Daksh <span className="sm:block hidden">Shukla</span></p>
+              <p className="text-white text-[18px] font-bold cursor-pointer flex">Daksh&nbsp;<span className="sm:block hidden">| Portfolio</span></p>
         </Link>
         
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -38,7 +38,7 @@ const Navbar = () => {
             <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
             <ul className="list-none flex justify-end items-start flex-col gap-4">
           {navLinks.map((link) => (
-            <li key={link.id} className={`${active===link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`} onClick={() => setActive(link.title)}>
+            <li key={link.id} className={`${active===link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursor-pointer text-[16px]`} onClick={() => {setToggle(!toggle); setActive(link.title);}}>
               <a href={`#${link.id}`}>{link.title}</a>
             </li>
           ))}
