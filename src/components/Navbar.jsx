ab6d2cb53f578ed96cbb14ac210ddf3e507f30c6
dakshshukla10 <123104147+dakshshukla10@users.noreply.github.com>
2023-08-17 12:27:33 +0530

    Feat:Added support for smaller frame devices , added toggle for menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import {logo,menu,close} from '../assets'
 
 const Navbar = () => {
   const [active, setActive] = useState('')
+  const [toggle, setToggle] = useState(false);
+
   return (
     <nav
     className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
@@ -29,6 +31,12 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+            <img src={toggle ? close : menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer" onClick={()=> setToggle(!toggle)} />
+            <div className={`${!toggle ? 'hidden' : 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}>
+
+            </div>
+        </div>
       </div>
     </nav>
   )
